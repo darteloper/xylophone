aae5e0df0b6db374c2e3aebc58adf754b64f5968
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-06-15 12:09:49 -0400

    Define application structure

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,35 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  Widget buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: RawMaterialButton(
+        fillColor: color,
+        onPressed: () {
+          print('play $soundNumber');
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.yellow, soundNumber: 2),
+              buildKey(color: Colors.orange, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
+        ),
       ),
     );
   }
