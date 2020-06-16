d4257edc884d2bc5f7bbaf5425bb7cf099e2ff79
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-06-15 23:21:26 -0400

    Update application design

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,58 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playSound(int soundNumber) {
+  void playSound(String soundNote) {
     final player = AudioCache();
-    player.play('note$soundNumber.wav');
+    player.play('note$soundNote.wav');
   }
 
-  Widget buildKey({Color color, int soundNumber}) {
+  Widget buildKey({Color primary, Color accent, String note, double spacing}) {
     return Expanded(
-      child: RawMaterialButton(
-        fillColor: color,
-        onPressed: () {
-          playSound(soundNumber);
-        },
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: spacing),
+        child: RawMaterialButton(
+          fillColor: primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(60.0),
+          ),
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 9, horizontal: 0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(50.0),
+              ),
+              border: Border.all(
+                color: accent,
+                width: 5.0,
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: accent,
+                  radius: 8.0,
+                ),
+                CircleAvatar(
+                  child: Text(
+                    note,
+                    style: TextStyle(
+                      color: primary,
+                    ),
+                  ),
+                  backgroundColor: accent,
+                ),
+                CircleAvatar(
+                  backgroundColor: accent,
+                  radius: 8.0,
+                ),
+              ],
+            ),
+          ),
+          onPressed: () {
+            playSound(note);
+          },
+        ),
       ),
     );
   }
@@ -24,17 +64,53 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white12,
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildKey(color: Colors.red, soundNumber: 1),
-              buildKey(color: Colors.yellow, soundNumber: 2),
-              buildKey(color: Colors.orange, soundNumber: 3),
-              buildKey(color: Colors.green, soundNumber: 4),
-              buildKey(color: Colors.teal, soundNumber: 5),
-              buildKey(color: Colors.blue, soundNumber: 6),
-              buildKey(color: Colors.purple, soundNumber: 7),
+              buildKey(
+                primary: Colors.red.shade800,
+                accent: Colors.redAccent.shade100,
+                note: 'DO',
+                spacing: 16,
+              ),
+              buildKey(
+                primary: Colors.deepOrange.shade700,
+                accent: Colors.deepOrangeAccent.shade100,
+                note: 'RE',
+                spacing: 24,
+              ),
+              buildKey(
+                primary: Colors.orange,
+                accent: Colors.orangeAccent.shade100,
+                note: 'MI',
+                spacing: 32,
+              ),
+              buildKey(
+                primary: Colors.green,
+                accent: Colors.greenAccent,
+                note: 'FA',
+                spacing: 40,
+              ),
+              buildKey(
+                primary: Colors.lightGreen,
+                accent: Colors.lightGreenAccent,
+                note: 'SOL',
+                spacing: 48,
+              ),
+              buildKey(
+                primary: Colors.lightBlue,
+                accent: Colors.lightBlueAccent.shade100,
+                note: 'LA',
+                spacing: 56,
+              ),
+              buildKey(
+                primary: Colors.deepPurple,
+                accent: Colors.deepPurpleAccent.shade100,
+                note: 'SI',
+                spacing: 64,
+              ),
             ],
           ),
         ),
